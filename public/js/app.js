df7e9b2e75bb1342e3e6a119df38d5daa4100c59
Joel <joel_1st@hotmail.com>
2015-03-14 18:03:05 +1000

    Finished basic front end routing, Added controllers and templates for new routes.

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,10 @@ var chatApp = angular.module('chatApp', ["ngRoute"]);
 chatApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 		when('/', { templateUrl: 'templates/chat-rooms.html', controller: "ChatRoomsCtrl" }).
+		when('/room/:id', { templateUrl: 'templates/chat-room.html', controller: "ChatRoomCtrl" }).
+		when('/create-room', { templateUrl: 'templates/create-room.html', controller: "CreateRoomCtrl" }).
+		when('/login-or-register', { templateUrl: 'templates/login-or-register.html', controller: "LoginOrRegisterCtrl" }).
+
 		otherwise({ redirectTo: '/' });
 }]);
 	
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,9 +14,34 @@ chatApp.controller('ChatRoomsCtrl', ["$scope", "Socket", function($scope, Socket
 		sock.emit('msg', $scope.sendMessage);
 		$scope.sendMessage = "";
 	};
+}]);
 
+chatApp.controller('ChatRoomCtrl', ["$scope", "$routeParams", "Socket", function($scope, $routeParams, Socket){
+	var sock = new Socket('fast-chat');
+	$scope.messages = [];
+	$scope.roomId = $routeParams.id;
+
+	sock.on('msg', function(data){
+		$scope.$apply(function(){
+			$scope.messages.push(data);
+		});
+		console.log(data)
+	});
+
+	$scope.submitMsg = function(){
+		sock.emit('msg', $scope.sendMessage);
+		$scope.sendMessage = "";
+	};
 }]);
 
 chatApp.controller('GuestNameCtrl', ["$scope", function($scope){
+	$scope.guestName = 'joel';
+}]);
+
+chatApp.controller('CreateRoomCtrl', ["$scope", function($scope){
+	$scope.guestName = 'joel';
+}]);
+
+chatApp.controller('LoginOrRegisterCtrl', ["$scope", function($scope){
 	$scope.guestName = 'joel';
 }]);
